e2d5257e64ad9bf0e4a7a41e21624d3b89a20ee3
diego@alpha <diegorodriguezriera@gmail.com>
2018-11-11 14:02:00 +0100

    fixed init.sh in symfony4 prod

--- symfony/symfony4.1/prod/init.sh
+++ symfony/symfony4.1/prod/init.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 composer install
 sed -i -r 's,(DATABASE_URL=).*,\1'"$DATABASE_URL"',' ./.env
+sed -i -r 's,(APP_ENV=).*,\1'"$ENV"',' ./.env
 sed -i -r 's,(.*)(driver: ).*,\1\2'"$DRIVER"',' ./config/packages/doctrine.yaml
 sed -i -r 's,(.*)( charset: ).*,\1\2'"$CHARSET"',' ./config/packages/doctrine.yaml
 sed -i -r 's,(.*)(collate: ).*,\1\2'"$COLLATE"',' ./config/packages/doctrine.yaml
